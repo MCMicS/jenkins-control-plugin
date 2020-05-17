7dd93bbedb9f45f28c9de32a4c333868a5432765
mcmics <MCMicS@users.noreply.github.com>
2020-05-17 12:53:39 +0200

    fix #208 Jenkins.LoadBuilds already registered Exception

--- src/main/java/org/codinjutsu/tools/jenkins/view/action/RunBuildAction.java
+++ src/main/java/org/codinjutsu/tools/jenkins/view/action/RunBuildAction.java
@@ -16,7 +16,6 @@
 
 package org.codinjutsu.tools.jenkins.view.action;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.progress.ProgressIndicator;
@@ -34,7 +33,6 @@ import org.codinjutsu.tools.jenkins.view.BrowserPanel;
 import org.codinjutsu.tools.jenkins.view.BuildParamDialog;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
@@ -44,67 +42,16 @@ public class RunBuildAction extends AnAction implements DumbAware {
 
     public static final String ACTION_ID = "Jenkins.RunBuild";
 
-    private static final Icon EXECUTE_ICON = AllIcons.Actions.Execute;
     private static final Logger LOG = Logger.getLogger(RunBuildAction.class.getName());
     public static final int BUILD_STATUS_UPDATE_DELAY = 1;
 
-    private final BrowserPanel browserPanel;
-
-
-    public RunBuildAction(BrowserPanel browserPanel) {
-        super("Build on Jenkins", "Run a build on Jenkins Server", EXECUTE_ICON);
-        this.browserPanel = browserPanel;
-    }
-
-
     @Override
-    public void actionPerformed(AnActionEvent event) {
+    public void actionPerformed(@NotNull AnActionEvent event) {
         final Project project = ActionUtil.getProject(event);
         final BrowserPanel browserPanel = ActionUtil.getBrowserPanel(event);
         try {
-            final Job job = browserPanel.getSelectedJob();
-            new Task.Backgroundable(project, "Running build", false) {
-
-                @Override
-                public void onSuccess() {
-                    notifyOnGoingMessage(job);
-                    ExecutorService.getInstance(project).getExecutor().schedule(new Runnable() {
-                        @Override
-                        public void run() {
-                            GuiUtil.runInSwingThread(() -> {
-                                final Optional<Job> newJob = browserPanel.getJob(job.getName());
-                                newJob.ifPresent(browserPanel::loadJob);
-                            });
-                        }
-                    }, BUILD_STATUS_UPDATE_DELAY, TimeUnit.SECONDS); //FIXME check delay coud be in settings
-
-                }
-
-                @Override
-                public void run(@NotNull ProgressIndicator progressIndicator) {
-                    progressIndicator.setIndeterminate(true);
-                    RequestManager requestManager = browserPanel.getJenkinsManager();
-                    if (job.hasParameters()) {
-                        BuildParamDialog.showDialog(job, JenkinsAppSettings.getSafeInstance(project), requestManager, new BuildParamDialog.RunBuildCallback() {
-
-                            public void notifyOnOk(Job job) {
-                                notifyOnGoingMessage(job);
-                                browserPanel.loadJob(job);
-                            }
-
-                            public void notifyOnError(Job job, Exception ex) {
-                                browserPanel.notifyErrorJenkinsToolWindow("Build '" + job.getName() + "' cannot be run: " + ex.getMessage());
-                                browserPanel.loadJob(job);
-                            }
-
-                        });
-
-                    } else {
-                        requestManager.runBuild(job, JenkinsAppSettings.getSafeInstance(project));
-                    }
-                }
-            }.queue();
-
+            Optional.ofNullable(browserPanel.getSelectedJob())
+                    .ifPresent(job -> queueRunBuild(project, browserPanel, job));
         } catch (Exception ex) {
             final String message = ex.getMessage() == null ? "Unknown error" : ex.getMessage();
             LOG.error(message, ex);
@@ -113,7 +60,8 @@ public class RunBuildAction extends AnAction implements DumbAware {
     }
 
     @Override
-    public void update(AnActionEvent event) {
+    public void update(@NotNull AnActionEvent event) {
+        final BrowserPanel browserPanel = ActionUtil.getBrowserPanel(event);
         Job selectedJob = browserPanel.getSelectedJob();
 
         final boolean isBuildable = selectedJob != null && selectedJob.isBuildable();
@@ -124,10 +72,47 @@ public class RunBuildAction extends AnAction implements DumbAware {
         }
     }
 
-
-    private void notifyOnGoingMessage(Job job) {
+    private void notifyOnGoingMessage(BrowserPanel browserPanel, Job job) {
         browserPanel.notifyInfoJenkinsToolWindow(HtmlUtil.createHtmlLinkMessage(
                 job.getName() + " build is on going",
                 job.getUrl()));
+    }
+
+    private void queueRunBuild(@NotNull Project project, @NotNull BrowserPanel browserPanel, @NotNull Job job) {
+        new Task.Backgroundable(project, "Running build", false) {
+
+            @Override
+            public void onSuccess() {
+                notifyOnGoingMessage(browserPanel, job);
+                ExecutorService.getInstance(project).getExecutor().schedule(() -> GuiUtil.runInSwingThread(() -> {
+                    final Optional<Job> newJob = browserPanel.getJob(job.getName());
+                    newJob.ifPresent(browserPanel::loadJob);
+                }), BUILD_STATUS_UPDATE_DELAY, TimeUnit.SECONDS);
+            }
+
+            @Override
+            public void run(@NotNull ProgressIndicator progressIndicator) {
+                progressIndicator.setIndeterminate(true);
+                RequestManager requestManager = browserPanel.getJenkinsManager();
+                if (job.hasParameters()) {
+                    BuildParamDialog.showDialog(job, JenkinsAppSettings.getSafeInstance(project), requestManager, new BuildParamDialog.RunBuildCallback() {
+
+                        public void notifyOnOk(Job job) {
+                            notifyOnGoingMessage(browserPanel, job);
+                            browserPanel.loadJob(job);
+                        }
+
+                        public void notifyOnError(Job job, Exception ex) {
+                            browserPanel.notifyErrorJenkinsToolWindow("Build '" + job.getName() + "' cannot be run: " + ex.getMessage());
+                            browserPanel.loadJob(job);
+                        }
+
+                    });
+
+                } else {
+                    requestManager.runBuild(job, JenkinsAppSettings.getSafeInstance(project));
+                }
+            }
+        }.queue();
     }
 }
