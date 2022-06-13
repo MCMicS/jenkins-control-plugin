fdb659f283a489d8941eee42eced67a726e1b177
mcmics <MCMicS@users.noreply.github.com>
2022-06-13 19:20:21 +0200

    Update API to 2022.2 eap (222.2964.55)

--- src/main/java/org/codinjutsu/tools/jenkins/view/action/RunBuildAction.java
+++ src/main/java/org/codinjutsu/tools/jenkins/view/action/RunBuildAction.java
@@ -16,9 +16,9 @@
 
 package org.codinjutsu.tools.jenkins.view.action;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.UpdateInBackground;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
@@ -43,7 +43,7 @@ import java.util.function.Consumer;
 
 import static org.codinjutsu.tools.jenkins.view.BrowserPanel.POPUP_PLACE;
 
-public class RunBuildAction extends AnAction implements DumbAware, UpdateInBackground {
+public class RunBuildAction extends AnAction implements DumbAware {
 
     public static final String ACTION_ID = "Jenkins.RunBuild";
     public static final int BUILD_STATUS_UPDATE_DELAY = 1;
@@ -70,6 +70,11 @@ public class RunBuildAction extends AnAction implements DumbAware, UpdateInBackg
             LOG.error(message, ex);
             browserPanel.notifyErrorJenkinsToolWindow("Build cannot be run: " + message);
         }
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
     }
 
     @Override
